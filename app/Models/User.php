6ce0805decce64f49d41ad9b4e3f3b74a384f0cd
Xuan Thuong <thuong.mai@monstar-lab.com>
2021-09-17 13:08:44 +0700

    feat: 1 -> 5 laravel security

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,8 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'role',
+        'status',
         'email',
         'password',
     ];
@@ -41,4 +43,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
